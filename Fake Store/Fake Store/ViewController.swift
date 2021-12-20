842b24c2530d57176a9558fe1daad17ee84950b5
Kirill Verkhoturov <kirill_verkhoturov@epam.com>
2021-12-20 03:35:16 +0300

    Added project files

---
+++ Fake Store/Fake Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fake Store
+//
+//  Created by Kirill Verkhoturov on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fake Store/Pods/Target Support Files/Pods-Fake Store-Fake StoreUITests/Pods-Fake Store-Fake StoreUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fake_Store_Fake_StoreUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Fake_Store_Fake_StoreUITests
+@end
