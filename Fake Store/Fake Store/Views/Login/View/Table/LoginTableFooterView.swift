021ef2febfa5e8579bc32652428e89bd8d45d31d
antonSob <anton_sobolev@epam.com>
2022-01-11 17:49:16 +0300

    style(FS-6-User-Api): delete commented-out code, update offset

--- Fake Store/Fake Store/Views/Login/View/Table/LoginTableFooterView.swift
+++ Fake Store/Fake Store/Views/Login/View/Table/LoginTableFooterView.swift
@@ -88,13 +88,7 @@ class LoginTableFooterView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupLayout()
-
     }
-
-//    override init(reuseIdentifier: String?) {
-//        super.init(reuseIdentifier: reuseIdentifier)
-//        setupLayout()
-//    }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
